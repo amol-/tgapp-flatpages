f927691ca53183609010842bf42df747078c4cec
Alessandro Molina <amol@Pulsar-3.local>
2015-04-23 23:46:16 +0200

    Forward compatibility with TG2.3.5

--- flatpages/__init__.py
+++ flatpages/__init__.py
@@ -6,5 +6,5 @@ import tg
 def plugme(app_config, options):
     config = {'templates': options.get('templates', [('genshi:flatpages.templates.page', 'default')]),
               'format': options.get('format', 'rst')}
-    tg.config['_flatpages'] = config
+    app_config['_flatpages'] = config
     return dict(appid='pages', global_helpers=False)
